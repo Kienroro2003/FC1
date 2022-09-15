690687302d2d0213dfcb8d30e321303066acc47c
Kienroro2003 <kienroro281@gmail.com>
2022-09-16 00:09:24 +0700

    lab02

---
+++ src/lab7_inheritance/lab02/Game.java
@@ -0,0 +1,27 @@
+package lab7_inheritance.lab02;
+
+public class Game {
+    private String description;
+
+    public Game() {
+    }
+
+    public Game(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "description='" + description + '\'' +
+                '}';
+    }
+}
